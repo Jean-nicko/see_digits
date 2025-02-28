2f0a34c51dab119f0317a43926da2bb82b50560e
FemCatWomen <perronfelixantoine@gmail.com>
2025-02-28 10:31:43 -0500

    Update see_digit_dummypart2.py
    update de see_digit_dummypart1

--- see_digit_dummypart2.py
+++ see_digit_dummypart2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+ 
 def main():
     data = pd.read_csv("C:/documents_local/`projet_math/data_csv/mnist_train.csv")
     data_testing_complete = pd.read_csv("C:/documents_local/`projet_math/data_csv/mnist_test.csv")
